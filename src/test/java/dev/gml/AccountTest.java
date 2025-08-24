ca55280cbc16782fb20c2a6122c517ec1635c928
J-JD <jamesjohndoe@protonmail.com>
2025-08-24 16:42:38 +0200

    feat(TDD cycle: Red stage): Add failing test for `withdraw` method.
    The new test describe a desired behavior for the `Account` class:
    "It should withdraw an amount of money from the account by updating
    its balance. The withdrawal amount must not exceed the balance".
    
    This test "should fail!", because the code to make it pass hasn't been
    written yet (`withdraw` method doesn't exist yet!).

--- src/test/java/dev/gml/AccountTest.java
+++ src/test/java/dev/gml/AccountTest.java
@@ -65,4 +65,47 @@ public class AccountTest {
     }
 
     // TODO: Continue the TDD cycle with the "withdraw" method.
+    @Test
+    @DisplayName("3.1. It should validate a successful withdrawal where the balance is sufficient.")
+    public void testWithdrawalWithSufficientFunds(){
+        // --- Given (aka Arrange) ---
+        // a parameterized account object, a withdrawal amount,
+        // an expected balance, and the total number of withdrawals.
+        Account account = new Account(200.0f, 5.0f);
+        float withdrawalAmount = 50.0f;
+        float expectedBalance = 150.0f;
+        int expectedWithdrawals = 1;
+
+        // --- When (aka Act) ---
+        // the withdrawal is executed.
+        account.withdraw(withdrawalAmount);
+
+        // --- Then (aka Assert) ---
+        // it correctly updates the account's balance,
+        // and increments the withdrawal count number.
+        assertEquals(expectedBalance, account.balance, "Balance should be updated correctly after a successful withdrawal.");
+        assertEquals(expectedWithdrawals, account.numberOfWithdrawals, "Number of withdrawals should be incremented.");
+    }
+
+    @Test
+    @DisplayName("3.2. It should NOT validate a withdrawal where the balance is insufficient.")
+    public void testWithdrawalWithInsufficientFunds(){
+        // --- Given (aka Prepare) ---
+        // a parameterized account object, a withdrawal amount,
+        // an expected balance, and the total number of withdrawals.
+        Account account = new Account(50.0f, 5.0f);
+        float withdrawalAmount = 100.0f;
+        float expectedBalance = 50.0f;
+        int expectedWithdrawals = 0;
+
+        // --- When (aka Act) ---
+        // the withdrawal is executed.
+        account.withdraw(withdrawalAmount);
+
+        // --- Then (aka Assert) ---
+        // as the withdrawal amount exceeds the current balance,
+        // it verifies that the balance and the number of withdrawals remain unchanged.
+        assertEquals(expectedBalance, account.balance, "Balance should not change with insufficient funds.");
+        assertEquals(expectedWithdrawals, account.numberOfWithdrawals, "Number of withdrawals should NOT be incremented.");
+    }
 }
